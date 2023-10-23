64ddd7eebf61f9178e688259ab13c2e9f009c4aa
luisotavio <luisotavio0553@gmail.com>
2023-10-23 08:16:44 -0300

    app.js mudado

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+
+app.set('port', process.env.PORT || 3333);
+
+module.exports = app;
+
 const router = require('./routes/rotas');
 
 app.use('/api', router);
